a7ea2f0630e5ff20479ee8d623049614bd4cf806
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-16 12:37:10 +0300

    Added 'previous song' button

--- js/Main.js
+++ js/Main.js
@@ -2,17 +2,9 @@ var song = document.getElementById("song");
 var icon = document.getElementById("icon");
 
 
-
-icon.onclick = function(){
-    if (song.paused) {
-        song.play();
-        icon.src = "./images/pause.png";
-    } else {
-        song.pause();
-        icon.src = "./images/play.png";
-    }
-}
+let i = 0;
 const songs = [
+    "./Songs/BoulevardofBrokenDreams.mp3",
     "./Songs/Heaven.mp3",
     "./Songs/Perfect.mp3",
     "./Songs/Bryan-Adams-Everything-I-Do-I-Do-It-For-You.mp3",
@@ -21,21 +13,37 @@ const songs = [
     "./Songs/DontSpeak.mp3",
     "./Songs/YouAreTheReason.mp3"];
 
-let i = 0;
+
+icon.onclick = function(){
+    if (song.paused) {
+        song.src = songs[i];
+        song.play();
+        icon.src = "./images/pause.png";
+    } else {
+        song.pause();
+        icon.src = "./images/play.png";
+    }
+}
+
 nextButton.onclick = function (){
     song.pause();
-    if(i>songs.length-1)
-    {
-        i=0
+    if(i==songs.length-1){
+        i=-1;
     }
-    song.src = songs[0+i];
     i++;
-    song.play();
-}
-/*nextButton.onclick = function (){
-    song.pause();
-    song.src = "./Songs/Bryan-Adams-Everything-I-Do-I-Do-It-For-You.mp3";
+    song.src = songs[i];
     icon.src = "./images/pause.png";
     song.play();
-    index++;
-}*/
+}
+
+prevButton.onclick = function(){
+    song.pause();
+    i--;
+    if(i==-1){
+        i = songs.length-1;
+    }
+    song.src = songs[i];
+    icon.src = "./images/pause.png";
+    song.play();
+}
+
