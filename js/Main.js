8a88122f17db95eca208ab77b2a67eb0ee94f1fd
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-16 10:03:51 +0300

    Added list of songs and a 'next button' to switch between them

--- js/Main.js
+++ js/Main.js
@@ -1,7 +1,9 @@
 var song = document.getElementById("song");
 var icon = document.getElementById("icon");
 
-icon.onclick = function (){
+
+
+icon.onclick = function(){
     if (song.paused) {
         song.play();
         icon.src = "./images/pause.png";
@@ -10,3 +12,27 @@ icon.onclick = function (){
         icon.src = "./images/play.png";
     }
 }
+const songs = [
+    "./Songs/Heaven.mp3",
+    "./Songs/Perfect.mp3",
+    "./Songs/Bryan-Adams-Everything-I-Do-I-Do-It-For-You.mp3",
+    "./Songs/YouAreTheReason.mp3"];
+
+let i = 0;
+nextButton.onclick = function (){
+    song.pause();
+    if(i>2)
+    {
+        i=0
+    }
+    song.src = songs[0+i];
+    i++;
+    song.play();
+}
+/*nextButton.onclick = function (){
+    song.pause();
+    song.src = "./Songs/Bryan-Adams-Everything-I-Do-I-Do-It-For-You.mp3";
+    icon.src = "./images/pause.png";
+    song.play();
+    index++;
+}*/
