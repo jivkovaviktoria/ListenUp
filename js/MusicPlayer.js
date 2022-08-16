dea874c1dc2617def02b915e911f316f7d9a2365
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-08-16 13:57:27 +0300

    Fixed the images files paths

--- js/MusicPlayer.js
+++ js/MusicPlayer.js
@@ -3,85 +3,85 @@ const songs = [
         songname: "Llamame",
         path: "/Songs/Llamame.mp3",
         artist: "WRS",
-        photo: "/images/llamame.webp"
+        photo: "../images/llamame.webp"
     },
     {
         songname: "Waves",
         path: "/Songs/Waves.mp3",
         artist: "Mr. Probz",
-        photo: "/images/waves.jpg"
+        photo: "../images/waves.jpg"
     },
     {
         songname: "Boulevard of broken dreams",
         path: "/Songs/BoulevardOfBrokenDreams.mp3",
         artist: "Green Day",
-        photo: "/images/boulevardOfBrokenDreams.jpg"
+        photo: "../images/boulevardOfBrokenDreams.jpg"
     },
     {
         songname: "Heaven",
         path: "/Songs/Heaven.mp3",
         artist: "Bryan Adams",
-        photo: "/images/heaven.jpg"
+        photo: "../images/heaven.jpg"
     },
     {
         songname: "Perfect",
         path: "/Songs/Perfect.mp3",
         artist: "Ed Sheeran",
-        photo: "/images/perfect.jpg"
+        photo: "../images/perfect.jpg"
     },
     {
         songname: "Everything I do",
         path: "/Songs/EverythingIDo.mp3",
         artist: "Bryan Adams",
-        photo: "/images/everythingIdo.jpg"
+        photo: "../images/everythingIdo.jpg"
     },
     {
         songname: "Nobody but you",
         path: "/Songs/NobodyButYou.mp3",
         artist: "Blake Shelton",
-        photo: "/images/nobodyButYou.jpg"
+        photo: "../images/nobodyButYou.jpg"
     },
     {
         songname: "Forever after all",
         path: "/Songs/ForeverAfterAll.mp3",
         artist: "Luke Combs",
-        photo: "/images/foreverAfterALl.jpg"
+        photo: "../images/foreverAfterALl.jpg"
     },
     {
         songname: "Don't speak",
         path: "/Songs/DontSpeak.mp3",
         artist: "No Doubt",
-        photo: "/images/dontSPeak.jpg"
+        photo: "../images/dontSPeak.jpg"
     },
     {
         songname: "You are the reason",
         path: "/Songs/YouAreTheReason.mp3",
         artist: "Calum Scott",
-        photo: "/images/youAreTheReason.jpg"
+        photo: "../images/youAreTheReason.jpg"
     },
     {
         songname: "Animal",
         path: "/Songs/Animal.mp3",
         artist: "Alvaro Soler",
-        photo: "/images/animal.jpg"
+        photo: "../images/animal.jpg"
     },
     {
         songname: "ZITTI E BUONI",
         path: "/Songs/ZittiEBuoni.mp3",
         artist: "Maneskin",
-        photo: "/images/zittiebuoni.jpg"
+        photo: "../images/zittiebuoni.jpg"
     },
     {
         songname: "Every breath you take",
         path: "/Songs/EveryBreathYouTake.mp3",
         artist: "The Police",
-        photo: "/images/everyBreathYouTake.jpg"
+        photo: "../images/everyBreathYouTake.jpg"
     },
     {
         songname: "Breaking Me",
         path: "/Songs/BreakingMe.mp3",
         artist: "Topic ft. A7S",
-        photo: "/images/breakingMe.png"
+        photo: "../images/breakingMe.png"
     }
 ];
 
